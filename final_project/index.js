c5b7a38c7e93c544a294bcaa936079a64c253aa8
Sinchana-M-R <156436712+Sinchana-M-R@users.noreply.github.com>
2024-11-21 23:36:30 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+ const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+
+  if (!token) {
+    return res.status(403).send('Token is required for authentication.');
+  }
+
+  // Verify the JWT token
+  jwt.verify(token, 'fingerprint_customer', (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid or expired token.');
+    }
+    // Store the user information in the request object for use in subsequent routes
+    req.user = user;
+    next();  // Continue to the next middleware or route handler
+  });
 });
  
 const PORT =5000;
