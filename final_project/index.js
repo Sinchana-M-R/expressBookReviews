ebd99648d09a1cd97ec899d2f11e779af16400bf
Sinchana-M-R <156436712+Sinchana-M-R@users.noreply.github.com>
2024-11-22 10:44:54 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,24 +10,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
- const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+app.use(
+    '/customer',
+    session({ secret: 'fingerprint_customer', resave: true, saveUninitialized: true })
+);
 
-  if (!token) {
-    return res.status(403).send('Token is required for authentication.');
-  }
-
-  // Verify the JWT token
-  jwt.verify(token, 'fingerprint_customer', (err, user) => {
-    if (err) {
-      return res.status(403).send('Invalid or expired token.');
+app.use('/customer/auth/*', function auth(req, res, next) {
+    const token = req.session.token;
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access!' });
     }
-    // Store the user information in the request object for use in subsequent routes
-    req.user = user;
-    next();  // Continue to the next middleware or route handler
-  });
+
+    jwt.verify(token, 'fingerprint_customer', (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Invalid token!' });
+        }
+        req.username = decoded.username;
+        next();
+    });
 });
+
  
 const PORT =5000;
 
